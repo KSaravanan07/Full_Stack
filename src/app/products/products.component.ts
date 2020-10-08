2cd570239eac451873c7d5cca75e3178c27f0bb3
KSaravanan07 <jamesnaan007@gmail.com>
2020-10-08 10:16:04 +0530

    Added HomePage

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { CategoryService } from '../category.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
@@ -6,8 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  categories$;
+  category: string;
 
-  constructor() { }
+  constructor(productService : ProductService, categoryService : CategoryService, route: ActivatedRoute) {
+    productService.getAll().pipe(switchMap((products : any) => {
+      this.products = products;
+      return route.queryParamMap;
+
+    })).
+    subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+      this.products.filter(p => p.payload.val().category === this.category) : this.products;
+    });
+
+    this.categories$ = categoryService.getCategories().snapshotChanges();
+  }
 
   ngOnInit(): void {
   }
